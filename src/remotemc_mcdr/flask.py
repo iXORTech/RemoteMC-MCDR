aacabcb675c08cd2dffd383b444016618d6606d6
Cubik65536 <cubik65536@cubik65536.top>
2022-03-25 21:28:15 -0400

    feat: Add ping route for Flask

--- src/remotemc_mcdr/flask.py
+++ src/remotemc_mcdr/flask.py
@@ -3,6 +3,7 @@ from waitress import serve
 
 from mcdreforged.api.all import *
 
+from remotemc_mcdr.util.html_response_util import *
 from remotemc_mcdr.util.i18n_util import *
 
 server: PluginServerInterface = ServerInterface.get_instance().as_plugin_server_interface()
@@ -12,6 +13,10 @@ flask_app = Flask(__name__)
 flask_app.config['JSON_AS_ASCII'] = False
 flask_app.config['JSONIFY_MIMETYPE'] = "application/json; charset=utf-8"
 flask_app.config['DEBUG'] = True
+
+@flask_app.route('/ping', methods=["GET"])
+def ping():
+    return HtmlResponseUtil.get_200_response("PONG!")
 
 @new_thread('Flask@RemoteMC-MCDR')
 def run_flask(host: str, port: int):
