261b832a24dffd8f7d828f12448d2ec8b70bdc02
Cubik65536 <cubik65536@cubik65536.top>
2022-06-26 22:27:08 -0400

    feat: Add i18n to plugin help message

--- src/remotemc_mcdr/commands/help_command.py
+++ src/remotemc_mcdr/commands/help_command.py
@@ -1,25 +1,24 @@
 import re
 
-from mcdreforged.api.all import *
-
 from remotemc_mcdr.constants import *
+
+from remotemc_mcdr.util.i18n_util import *
 
 
 def show_help(source: CommandSource):
-    help_msg_lines = '''
--------------------- RemoteMC-MCDR v{0} --------------------
-MCDReforged Plugin of RemoteMC Series
-§7!!remotemc §rShow this help message
-§7!!msg §3<Message> §rSend message to all Minecraft servers and chat groups in the network
-§7!!broadcast §3<Message> §rBroadcast to all Minecraft servers and chat groups in the network
-'''.format(VERSION).splitlines(True)
+    help_msg_lines = ['\n',
+                      i18n('in_game.help_message.line1', VERSION) + '\n',
+                      i18n('in_game.help_message.line2') + '\n',
+                      i18n('in_game.help_message.line3') + '\n',
+                      i18n('in_game.help_message.line4') + '\n',
+                      i18n('in_game.help_message.line5') + '\n']
     help_msg_rtext = RTextList()
     for line in help_msg_lines:
         result = re.search(r'(?<=§7)!![\w ]*(?=§)', line)
         if result is not None:
             help_msg_rtext.append(
                 RText(line).c(RAction.suggest_command, result.group()).h(
-                    'Click to use command §7{}§r'.format(result.group())))
+                    i18n('in_game.click_to_use_command', result.group())))
         else:
             help_msg_rtext.append(line)
     source.reply(help_msg_rtext)
