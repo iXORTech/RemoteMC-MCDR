2c3110d75c3b5a58d292ea209e39e1a470a47dd8
Cubik65536 <cubik65536@cubik65536.top>
2022-08-09 15:53:22 -0400

    feat: Add permission control to `!!msg` and `!!broadcast` commands

--- src/remotemc_mcdr/constants.py
+++ src/remotemc_mcdr/constants.py
@@ -2,6 +2,6 @@ from mcdreforged.api.all import PluginServerInterface, ServerInterface
 
 server: PluginServerInterface = ServerInterface.get_instance().as_plugin_server_interface()
 
-CONTROL_COMMAND_PREFIX = "!!remotemc"
-MESSAGE_COMMAND_PREFIX = "!!msg"
-BROADCAST_COMMAND_PREFIX = "!!broadcast"
+CONTROL_COMMAND_PREFIX = "remotemc"
+MESSAGE_COMMAND_PREFIX = "msg"
+BROADCAST_COMMAND_PREFIX = "broadcast"
