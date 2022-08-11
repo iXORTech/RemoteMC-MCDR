c6fdc45c16b0780226a8e16587015c70cd1ce52c
Cubik65536 <cubik65536@cubik65536.top>
2022-08-11 14:32:10 -0400

    feat(utils/version_util): Add log outputs

--- src/remotemc_mcdr/util/version_util.py
+++ src/remotemc_mcdr/util/version_util.py
@@ -1,7 +1,7 @@
 import json
 import re
 
-from mcdreforged.api.all import *
+from remotemc_mcdr.util.i18n_util import *
 
 server: PluginServerInterface = ServerInterface.get_instance().as_plugin_server_interface()
 version_properties = None
@@ -11,13 +11,17 @@ def load_version_properties():
     global version_properties
     with server.open_bundled_file(f"remotemc_mcdr/version.json") as f:
         version_properties = json.load(f)
+    server.logger.info(i18n("version_util.file_loaded"))
 
 
 def get_version_property(value: str) -> str:
+    server.logger.info(i18n("version_util.getting_version_property",
+                            value, version_properties[value]))
     return version_properties[value]
 
 
 def get_version() -> str:
+    server.logger.info(i18n("version_util.getting_version"))
     version_property = get_version_property("version")
     stage_property = get_version_property("stage")
     stage_property = re.sub(r"dev", "DEV", stage_property)
@@ -29,4 +33,6 @@ def get_version() -> str:
     stage_property = re.sub(r"rc", "Release Candidate", stage_property)
     revision_property = get_version_property("revision")
     revision_property = revision_property.upper()
-    return f"{version_property} {stage_property} ({revision_property})"
+    version_property = f"{version_property} {stage_property} ({revision_property})"
+    server.logger.info(i18n("version_util.got_version", version_property))
+    return version_property
