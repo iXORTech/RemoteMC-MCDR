a0e3560e0abc68960bcb145d78fe1cac1c1f360b
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-05-06 00:50:02 -0400

    feat: Register `!!remotemc status` command

---
+++ src/remotemc_mcdr/commands/status_command.py
@@ -0,0 +1,9 @@
+from remotemc_mcdr.util.config_util import *
+from remotemc_mcdr.util.i18n_util import *
+
+server: PluginServerInterface = ServerInterface.get_instance().as_plugin_server_interface()
+config: Configure = load_config(server)
+
+
+def status_command(source: CommandSource):
+    source.reply("STATUS_COMMAND_REPLY")
