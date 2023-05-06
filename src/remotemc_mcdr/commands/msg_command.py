38d41190ccec50e85a779e0fc112eabdc000bd99
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2023-05-06 00:49:15 -0400

    fix: Fix `remotemc_core_response_list` read

--- src/remotemc_mcdr/commands/msg_command.py
+++ src/remotemc_mcdr/commands/msg_command.py
@@ -44,7 +44,7 @@ def send_message(source: CommandSource, context: dict):
 
     for response in remotemc_core_response_list:
         status_code = response["statusCode"]
-        message = response["message"]
+        message = response["body"]
         if status_code == 200:
             server.logger.info(i18n("message_and_broadcast.sucessful_execution_response_received",
                                     i18n("message"), status_code, message))
